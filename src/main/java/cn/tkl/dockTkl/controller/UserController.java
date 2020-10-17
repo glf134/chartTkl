ee0718d75d887a5cce64e01e6c7b7d2733179959
glf <1347665919@qq.com>
2020-10-17 13:57:29 +0800

    1

--- src/main/java/cn/tkl/dockTkl/controller/UserController.java
+++ src/main/java/cn/tkl/dockTkl/controller/UserController.java
@@ -3,10 +3,13 @@ package cn.tkl.dockTkl.controller;
 import cn.tkl.dockTkl.pojo.User;
 import cn.tkl.dockTkl.service.impl.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("user")
@@ -23,5 +26,12 @@ public class UserController {
     @GetMapping("hello")
     public String test(){
         return "hello ssm";
+    }
+
+    @GetMapping("/list")
+    public List<User> all(ModelMap model) {
+        // 查询用户
+        List<User> users = this.userService.queryAll();
+        return users;
     }
 }
--- src/main/java/cn/tkl/dockTkl/service/impl/UserService.java
+++ src/main/java/cn/tkl/dockTkl/service/impl/UserService.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 public class UserService {
 
@@ -15,9 +17,13 @@ public class UserService {
     public User queryById(Long id){
         return this.userMapper.selectByPrimaryKey(id);
     }
+    public List<User> queryAll(){
+        return this.userMapper.selectAll();
+    }
 
     @Transactional
     public void deleteById(Long id){
         this.userMapper.deleteByPrimaryKey(id);
     }
+
 }
